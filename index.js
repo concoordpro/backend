cf6ddba3b9c2b29479c0a2d1448f7b734f28173e
concoordpro <concoordweb@outlook.com>
2024-07-10 08:00:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Repositorio do Concoord, Backend\n'
-  const msg = 'Repositorio do Concoord, Backend 2\n'
+  const msg = 'Repositorio do Concoord, Backend 3\n'
 
   res.end(msg);
 });
