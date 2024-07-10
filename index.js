e8ca9ba1c9a0430c7a43c641c0058d329f683e98
concoordpro <concoordweb@outlook.com>
2024-07-10 08:10:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Repositorio do Concoord, Backend\n'
-  const msg = 'Repositorio do Concoord, Backend 3\n'
+
+  const msg = 'Repositorio do Concoord, Backend 10/07/2024 08:09\n'
 
   res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
